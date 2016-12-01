61dcce0f39500a7c1edc06abe0d68174276a1b96
John Iannandrea <jiannandrea@gmail.com>
2016-11-30 20:42:18 -0500

    added ffmpy logic TODO: make it work ;p

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name='camera',
+    description='',
+    author='John Iannandrea',
+    author_email='jiannandrea@gmail.com',
+    url='http://github.com/isivisi/camera',
+    install_requires=[
+        'ffmpy',
+        'picamera',
+        'opencv-python'
+    ],
+    include_package_data=True,
+    version='0.1.4',
+    packages=['raspcam'],
+    zip_safe=False,
+    entry_points={
+        'console_scripts': [
+            'pycam = pycam:main'
+        ]
+    }
+)
